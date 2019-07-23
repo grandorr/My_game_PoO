83e4565631c4dc7fa478dadc51831fbf4593c75e
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-07-23 18:24:01 +0200

    sa avance bien !

--- app3.rb
+++ app3.rb
@@ -3,4 +3,18 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
-my_game = Game.new("Wolverine")
+puts " Bienvenue dans mon super jeu !!! "
+puts " Appuie sur une touche quand tu es pret"
+gets.chomp
+puts " Très bien, let's go "
+puts " Quel est le nom que tu veux donner à ton personnage ?"
+name_user = gets.chomp
+my_game = Game.new(name_user)
+while my_game.is_still_ongoing? == false
+	puts "__________________________________________"
+	my_game.show_players
+	my_game.menu 
+	my_game.menu_choice(gets.chomp)
+	my_game.enemies_attack
+end
+my_game.end
--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,6 @@ class Game
 		@human_player = HumanPlayer.new(name_to_save)
 		@enemies = [player_1 = Player.new("Zombie1"),player_2 = Player.new("Zombie2"),
 		player_3 = Player.new("Zombie3"),player_4 = Player.new("Zombie4")]
-		is_still_ongoing?
 	end
 
 	def kill_player(enemies_to_kill)
@@ -38,33 +37,66 @@ class Game
 		puts "attaquer un joueur en vue :"
 		n = 1 
 		@enemies.each do |enemie|
-			puts "#{n} - enemie.show_state"
+			print " #{n} - " 
+			puts "#{enemie.show_state}"
 			n += 1 
 		end
+		puts " ATTENTION : Si tu rentre autre chose que ce qui est proposé, tu passe ton tour !!!"
 	end
 
 	def menu_choice(result_choice)
 		if result_choice == "a"
 			@human_player.search_weapon
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp
 		elsif result_choice == "s"
 			@human_player.search_health_pack
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp
 		elsif result_choice == "1"
 			@human_player.attacks(@enemies[0])
 			if @enemies[0].life_points == 0
 				kill_player(@enemies[0])
+			end
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp
 		elsif result_choice == "2"
 			@human_player.attacks(@enemies[1])
 			if @enemies[1].life_points == 0
-				kill_player(@enemies[1])			
+				kill_player(@enemies[1])
+			end	
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp		
 		elsif result_choice == "3"
 			@human_player.attacks(@enemies[2])
 			if @enemies[2].life_points == 0
 				kill_player(@enemies[2])
+			end
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp
 		elsif result_choice == "4"
 			@human_player.attacks(@enemies[3])
 			if @enemies[3].life_points == 0
 				kill_player(@enemies[3])
+			end
+			puts "Appuie sur une touche pour continuer"
+			gets.chomp
+		end
+	end
+  
+  def enemies_attack
+  	@enemies.each do |enemie|
+  		enemie.attacks(@human_player)
+  	end
+  end
 
+  def end 
+  	if @human_player.life_points == 0
+		puts " Looser tu as perdu..."
+		elsif @enemies.empty? == true
+		puts " Bravo mec t'es trop fort "
+		end
+	end
 
 
 
